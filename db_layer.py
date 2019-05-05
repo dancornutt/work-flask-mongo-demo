6c1c94ea2378c3effe359bd4359e057af8a8d7ee
dancornutt <42428640+dancornutt@users.noreply.github.com>
2019-05-05 10:07:03 -0700

    Update db_layer.py

--- db_layer.py
+++ db_layer.py
@@ -1,7 +1,7 @@
 import pymongo
 
 mongo = pymongo.MongoClient(
-    'mongodb+srv://kiwi-admin:Test777Boeing@kiwi-jwnc9.azure.mongodb.net/test?retryWrites=true', maxPoolSize=50, connect=False)
+    'mongodb+srv://{user}:{user_pw}@kiwi-jwnc9.azure.mongodb.net/test?retryWrites=true', maxPoolSize=50, connect=False)
 
 db = pymongo.database.Database(mongo, 'irc_db')
 col = pymongo.collection.Collection(db, 'parts')
