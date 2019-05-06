7638c68588f929cd55bb116bb693f6ea94ac6b29
Zack Connaughton <zaconnaughton@gmail.com>
2019-05-05 18:44:13 -0700

    updated base.jinja2 to not cache the css for the page

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 
+config = {
+    "DEBUG": True,
+    "CACHE_TYPE": "null",
+    "CACHE_DEFAULT_TIMEOUT": 1,
+    "MONGO_URI": "mongodb+srv://kiwi-admin:Test777Boeing@kiwi-jwnc9.azure.mongodb.net/test?retryWrites=true"
+}
+
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://kiwi-admin:Test777Boeing@kiwi-jwnc9.azure.mongodb.net/test?retryWrites=true"
+#app.config["MONGO_URI"] = "mongodb+srv://kiwi-admin:Test777Boeing@kiwi-jwnc9.azure.mongodb.net/test?retryWrites=true"
+app.config.from_mapping(config)
 mongo = PyMongo(app)
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home_page():
-    # online_users = mongo.db.users.find({'online': True})
-    # return render_template('index.html', online_users=online_users)
-    options = ["Part", "Assembly", "Installation"]
-    return render_template("main_page.jinja2", options=options)
+    if request.method == 'POST':
+        print('POST Request')
+        print(request.form.get('search_type'))
+    elif request.method == "GET":
+        options = ["Part", "Assembly", "Installation"]
+        return render_template("main_page.jinja2", options=options)
 
 @app.route('/part/<int:part_id>')
 def part_page(part_id):
