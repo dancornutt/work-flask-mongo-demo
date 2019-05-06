641ea12cd009f8e4879936ed3d9b260646acf31e
Zack Connaughton <zaconnaughton@gmail.com>
2019-05-06 08:32:52 -0700

    removed commented lines of code from the file used to copy main.py. Removed the app.config mongo as that will be handled with the database layer

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ from flask_pymongo import PyMongo
 config = {
     "DEBUG": True,
     "CACHE_TYPE": "null",
-    "CACHE_DEFAULT_TIMEOUT": 1,
-    "MONGO_URI": "mongodb+srv://kiwi-admin:Test777Boeing@kiwi-jwnc9.azure.mongodb.net/test?retryWrites=true"
+    "CACHE_DEFAULT_TIMEOUT": 1
 }
 
 app = Flask(__name__)
-#app.config["MONGO_URI"] = "mongodb+srv://kiwi-admin:Test777Boeing@kiwi-jwnc9.azure.mongodb.net/test?retryWrites=true"
 app.config.from_mapping(config)
 mongo = PyMongo(app)
 
@@ -23,14 +21,10 @@ def home_page():
 def part_page():
     print(request.args)
     part_id = request.args["part_number"]
-    # online_users = mongo.db.users.find({'online': True})
-    # return render_template('index.html', online_users=online_users)
     return render_template("part.jinja2",part=part_id)
 
 @app.route('/part/<int:part_id>')
 def part_page_specific(part_id):
-    # online_users = mongo.db.users.find({'online': True})
-    # return render_template('index.html', online_users=online_users)
     return render_template("part.jinja2", part=part_id)
 
 
