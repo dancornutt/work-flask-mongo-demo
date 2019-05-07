273d593b8399aa785b7c5027c5c120dc3148c503
Zack Connaughton <zaconnaughton@gmail.com>
2019-05-07 09:00:03 -0700

    Updated some page display attributes and table display

--- static/js/main_page_js.js
+++ static/js/main_page_js.js
@@ -15,3 +15,21 @@ function myFunction() {
         }
     }
 }
+
+function PartFilter() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("select_Part_Type");
+    filter = input.value;
+    table = document.getElementById("PartTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+            if (td.innerHTML.indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
